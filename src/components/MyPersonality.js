bcacc5cc50a0898a71e228c8df7ce956c4cd2f21
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-11-10 07:03:54 +0000

    Final touches

--- src/components/MyPersonality.js
+++ src/components/MyPersonality.js
@@ -5,14 +5,6 @@ const PersonalityComponent = () => {
   return (
     <div className="meet-team-member-right">
       <div>
-        <div>
-          <div
-            className="allcaps-sml red-emphasis-text"
-            style={{ marginTop: 20, marginLeft: 65 }}
-          >
-            You can expect
-          </div>
-        </div>
         <div
           className="_80-width-header"
           style={{ marginTop: 20, marginLeft: 65 }}
