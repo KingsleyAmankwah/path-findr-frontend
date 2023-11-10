999ad949688ca744c65307f064c3588adfd48358
KingsleyAmankwah <kuamiklassic149@gmail.com>
2023-11-09 18:53:20 -0800

    😢

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} /> {/* Route for the root path */}
         {/* <Route path="/Home" element={<Home />} /> */}
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/mentor" element={<Mentor />} />
         <Route path="/survey" element={<Survey />} />
         <Route path="/after-survey" element={<AfterSurvey />} />
-        <Route path='/chat-mentor' element={<ChatApp />} />
+        <Route path='/chat-mentor' element={<ChatApp />} /> */}
       </Routes>
       <ToastContainer />
     </Router>
